312e25ff531fc80e3dd3acae8245c92b565b818b
Johnny Gérard <john@jgerard.dev>
2026-04-19 22:44:33 +0200

    feat: minor improvements to server action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -6,11 +6,8 @@ import { verifyTurnstileToken } from "~/api/verify-turnstile-token";
 import { emailSchema, messageSchema, nameSchema } from "~/validation/schemas";
 
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
-const MISSING_CF_TURNSTILE_TOKEN_ERROR =
-  "CAPTCHA verification failed. Please try again.";
 
 export const server = {
-  // The following server action will be used with a basic contact form.
   send: defineAction({
     accept: "form",
     input: z.object({
@@ -18,14 +15,12 @@ export const server = {
       name: nameSchema,
       email: emailSchema,
       message: messageSchema,
-      "cf-turnstile-response": z
-        .string(MISSING_CF_TURNSTILE_TOKEN_ERROR)
-        .min(1, MISSING_CF_TURNSTILE_TOKEN_ERROR),
+      "cf-turnstile-response": z.string().min(1),
     }),
-    handler: async (input) => {
+    handler: async (input): Promise<CreateEmailResponseSuccess> => {
       if (input.website) {
-        console.log("Received non-empty honeypot field.");
-        return { id: uuid() } satisfies CreateEmailResponseSuccess;
+        console.info("Received non-empty honeypot field.");
+        return { id: uuid() };
       }
 
       await verifyTurnstileToken(input["cf-turnstile-response"]);
@@ -42,7 +37,7 @@ export const server = {
           `Failed to send email (type: ${error.name}, status: ${error.statusCode}): ${error.message}`,
         );
         throw new ActionError({
-          code: ActionError.statusToCode(error.statusCode ?? 0),
+          code: ActionError.statusToCode(error.statusCode ?? 500),
           message: "Failed to send email. Please try again later.",
         });
       }
